23208244e08e56949931d352f038e3098b91159a
Philippe Gris <philippe.gris@clermont.in2p3.fr>
2019-04-02 13:24:03 +0200

    updating setup.py to add dependencies

--- setup.py
+++ setup.py
@@ -13,10 +13,12 @@ setup(
     python_requires='>=3.5',
     zip_safe=False,
     install_requires=[
-        'sn_tools',
+        'sn_tools>=0.1',
+        'sn_stackers>=0.1'
     ],
     dependency_links=[
-        'git+https://github.com/lsstdesc/sn_tools.git@dev'
+        'git+https://github.com/lsstdesc/sn_tools.git@dev#egg=sn_tools-0.1',
+        'git+https://github.com/lsstdesc/sn_stackers.git@dev#egg=sn_stackers-0.1',
     ],
 
 )
