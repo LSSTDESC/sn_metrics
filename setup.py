951cbbe4f96f951302667f29e626f1d7e21a2e56
pgris <gris@clermont.in2p3.fr>
2020-07-15 09:26:37 +0200

    add version number in __init__

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='sn_metrics',
-    version='v1.0.0',
+    version=__version__,
     description='Metrics for supernovae',
     url='http://github.com/lsstdesc/sn_metrics',
     author='Philippe Gris',
