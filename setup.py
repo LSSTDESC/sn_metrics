1c0019219578db6452168383db438dbc25b6f615
pgris <gris@clermont.in2p3.fr>
2020-07-15 11:52:11 +0200

    try to define a coherent versioning of the package

--- setup.py
+++ setup.py
@@ -1,9 +1,14 @@
 from setuptools import setup
 
+# get the version here
+pkg_vars  = {}
+
+with open("version.py") as fp:
+    exec(fp.read(), pkg_vars)
 
 setup(
     name='sn_metrics',
-    version=__version__,
+    version= pkg_vars['__version__'],
     description='Metrics for supernovae',
     url='http://github.com/lsstdesc/sn_metrics',
     author='Philippe Gris',
