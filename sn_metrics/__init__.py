b51f6bef6b42585da711cd9be94a411d9de553ed
pgris <gris@clermont.in2p3.fr>
2022-07-27 10:44:53 +0200

    modifs required for rubin_sim

--- sn_metrics/__init__.py
+++ sn_metrics/__init__.py
@@ -1,1 +1,18 @@
 from .version import __version__
+
+import os
+
+# requested infos for throughputs
+throughputs_dir = os.path.join(os.getenv("PWD"), "throughputs")
+if not os.path.isdir(throughputs_dir):
+    cmd = 'git clone https://github.com/lsst/throughputs'
+    os.system(cmd)
+
+os.environ['LSST_THROUGHPUTS_BASELINE'] = '{}/{}'.format(
+    throughputs_dir, 'baseline')
+os.environ['THROUGHPUTS_DIR'] = throughputs_dir
+
+os.environ['RUBIN_SIM_DATA_DIR'] = os.getenv("PWD")
+
+print('Reading throughputs from',
+      os.environ['LSST_THROUGHPUTS_BASELINE'], os.environ['THROUGHPUTS_DIR'])
