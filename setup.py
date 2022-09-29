de7cff778b9aba9ab14800b636c71be8118fface
Philippe Gris <gris@clermont.in2p3.fr>
2022-09-29 11:05:32 +0200

    install sn_metrics_input

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setup(
     author='Philippe Gris',
     author_email='philippe.gris@clermont.in2p3.fr',
     license='BSD',
-    packages=['sn_metrics'],
+    packages=['sn_metrics', 'sn_metrics_input'],
     python_requires='>=3.5',
     zip_safe=False,
     install_requires=[
