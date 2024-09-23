9e50442094e1ee3c5ae36e3f4fa67caad310e5f9
pgris <gris@clermont.in2p3.fr>
2024-09-23 10:35:44 +0200

    update

--- sn_metrics/sn_obs_strat_pixels.py
+++ sn_metrics/sn_obs_strat_pixels.py
@@ -7,12 +7,13 @@ Created on Fri Sep 20 16:46:37 2024
 """
 
 
-class SNObsStratMetric:
-    def __init__(self):
+class SNObsStratPixel:
+    def __init__(self, outDir, outName):
 
-        self.name = 'sn_obs_strat_metric'
+        self.name = 'sn_obs_strat_pixels'
         print('instance of', self.name)
 
     def run(self, dataSlice,  imulti=0):
 
-        print('running', len(dataSlice))
+        print('running', len(dataSlice),
+              dataSlice[['healpixID', 'pixRA', 'pixDec']])
